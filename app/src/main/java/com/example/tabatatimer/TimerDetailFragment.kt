1b5d5e50b8bf7a77fc40f052ad1d0c3c56de920d
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-12-19 19:50:24 +0300

    TimerDetailFragment observing

--- app/src/main/java/com/example/tabatatimer/TimerDetailFragment.kt
+++ app/src/main/java/com/example/tabatatimer/TimerDetailFragment.kt
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
+import com.example.tabatatimer.viewmodels.TimerDetailViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class TimerDetailFragment : Fragment() {
 
-    private val viewModel: TimerDetailFragment by activityViewModels()
+    private val viewModel: TimerDetailViewModel by activityViewModels()
     private lateinit var navBar: BottomNavigationView
 
     override fun onCreateView(
@@ -26,6 +28,18 @@ class TimerDetailFragment : Fragment() {
 
         val button: Button = view.findViewById(R.id.timer_submit_button)
         button.setOnClickListener(::onSubmitClick)
+
+        val title: EditText = view.findViewById(R.id.timer_title_text)
+        val preparation: EditText = view.findViewById(R.id.timer_preparation_text)
+        val workout: EditText = view.findViewById(R.id.timer_workout_text)
+        val rest: EditText = view.findViewById(R.id.timer_rest_text)
+        val cycles: EditText = view.findViewById(R.id.timer_cycles_text)
+
+        viewModel.title.observe(viewLifecycleOwner, { newValue -> title.setText(newValue) })
+        viewModel.preparation.observe(viewLifecycleOwner, { newValue -> preparation.setText(newValue)})
+        viewModel.workout.observe(viewLifecycleOwner, { newValue -> workout.setText(newValue)})
+        viewModel.rest.observe(viewLifecycleOwner, { newValue -> rest.setText(newValue)})
+        viewModel.cycles.observe(viewLifecycleOwner, { newValue -> cycles.setText(newValue)})
 
         return view
     }
