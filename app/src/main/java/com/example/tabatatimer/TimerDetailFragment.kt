87c16d247a1ff129cdabd5c70e9e58e75ddb1a51
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-12-19 17:32:41 +0300

    TimerDetailFragment layout init

--- app/src/main/java/com/example/tabatatimer/TimerDetailFragment.kt
+++ app/src/main/java/com/example/tabatatimer/TimerDetailFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 
 class TimerDetailFragment : Fragment() {
+
+    private val viewModel: TimerDetailFragment by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
