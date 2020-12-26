790d183c867ba3cd0c4cde7bda05949dd7c9fc0d
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-12-26 13:16:10 +0300

    colour spinner init

--- app/src/main/java/com/example/tabatatimer/SequenceDetailFragment.kt
+++ app/src/main/java/com/example/tabatatimer/SequenceDetailFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 
 class SequenceDetailFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_sequence_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_sequence_detail, container, false)
+
+        val colour: Spinner = view.findViewById<Spinner>(R.id.sequence_colour)
+        val adapter = ArrayAdapter.createFromResource(requireActivity().applicationContext, R.array.colours, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        colour.adapter = adapter
+
+
+        return view
     }
 
     companion object {
